f2971f1d3d03c300c9be5e7d5ad525362945f8ed
Bill Nottingham <notting@nostromo.devel.redhat.com>
2007-05-16 01:17:51 -0400

    add treediff. bump release.

--- mash.spec
+++ mash.spec
@@ -1,7 +1,7 @@
 %{!?python_sitelib: %define python_sitelib %(%{__python} -c "from distutils.sysconfig import get_python_lib; print get_python_lib()")}
 
 Name:           mash
-Version:        0.1.6
+Version:        0.1.7
 Release:        1%{?dist}
 Summary:        Buildsystem -> tree converter
 Group:          Development/Tools
@@ -40,6 +40,6 @@ rm -rf $RPM_BUILD_ROOT
 %config(noreplace) %{_sysconfdir}/mash
 %{python_sitelib}/mash
 %{_bindir}/*
-
+%{_datadir}/mash
 
 %changelog
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 import glob
 
 setup(name='mash',
-      version='0.1.6',
+      version='0.1.7',
       description='Build system -> repository tool',
       author='Bill Nottingham',
       author_email='notting@redhat.com',
@@ -10,6 +10,6 @@ setup(name='mash',
       license='GPL',
       packages=['mash'],
       scripts = ['mash.py'],
-      data_files=[('/etc/mash', glob.glob('configs/*'))]
+      data_files=[('/etc/mash', glob.glob('configs/*')),('/usr/share/mash',glob.glob('utils/*'))]
       )
 

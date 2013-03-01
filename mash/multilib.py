f96209ff9daa6d5d16efa13d76c8779b95ec7a61
Bill Nottingham <notting@redhat.com>
2013-03-01 11:18:21 -0500

    Drop pulseaudio-utils; shouldn't be needed any more.

--- mash/multilib.py
+++ mash/multilib.py
@@ -78,7 +78,7 @@ class RuntimeMultilibMethod(MultilibMethod):
     def select(self, po):
         libdirs = [ '/usr/lib', '/usr/lib64', '/lib', '/lib64' ]
         blacklist = [ 'tomcat-native' ]
-        whitelist = [ 'libgnat', 'wine', 'lmms-vst', 'nspluginwrapper', 'libflashsupport', 'pulseaudio-utils', 'valgrind', 'perl-libs', 'redhat-lsb', 'yaboot' ]
+        whitelist = [ 'libgnat', 'wine', 'lmms-vst', 'nspluginwrapper', 'libflashsupport', 'valgrind', 'perl-libs', 'redhat-lsb', 'yaboot' ]
         if po.name in blacklist:
             return False
         if po.name in whitelist:

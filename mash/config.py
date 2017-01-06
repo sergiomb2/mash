467b0cf4290efdd4cd356c2a6dc4d73c308ba7cc
Mohan Boddu <mboddu@redhat.com>
2017-01-06 12:12:03 -0500

    Adding postgresql-odbc and mysql-connector-odbc to multilib whitelist
    Signed-off-by: Mohan Boddu <mboddu@redhat.com>

--- mash/config.py
+++ mash/config.py
@@ -87,7 +87,7 @@ class MashConfig(config.BaseConfig):
         'libgnat', 'wine', 'lmms-vst', 'nspluginwrapper',
         'libflashsupport', 'valgrind', 'perl-libs', 'redhat-lsb',
         'yaboot', 'syslinux-extlinux-nonlinux', 'syslinux-nonlinux',
-        'syslinux-tftpboot', 'nosync',
+        'syslinux-tftpboot', 'nosync', 'postgresql-odbc', 'mysql-connector-odbc'
     )))
     multilib_runtime_blacklist = SetOption(set((
         'tomcat-native', 'php', 'httpd',
